67cda24f7236de5f7b89f9edcc65f5d185245c6b
Vijaya <srigowri.venkatesha@gmail.com>
2020-08-02 19:40:40 +0530

    changed the fuction completely

--- assets/js/carousel_custom.js
+++ assets/js/carousel_custom.js
@@ -1,30 +1,21 @@
 
-var create_carousel = (function (window) {
-    var slideIndex = 0;
-    startCarousel();
-    var startCarousel = function () {
-        var i;
-        var slides = document.getElementsByClassName("carousel_img");
-        var dots = document.getElementsByClassName("carousel_dot");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}    
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" carousel_active", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " carousel_active";
-        setTimeout(startCarousel, 2000); // Change image every 2 seconds
+ var startCarousel = function() {
+    var i;
+    var slides = document.getElementsByClassName("carousel_img");
+    var dots = document.getElementsByClassName("carousel_dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
-
-    // Expose methods.
-    return {
-        startCarousel: startCarousel
-    };
-
-})(window);
-
-// Kickstart Demo.
-window.onload = create_carousel.startCarousel;
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" carousel_active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " carousel_active";
+    setTimeout(startCarousel, 2000); // Change image every 2 seconds
+};
+window.onload = function(){
+ var slideIndex = 0;
+ new startCarousel();
+}
