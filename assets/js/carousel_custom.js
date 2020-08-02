e5db2578fb1103c26a4954fb2ec6531d11b85097
Vijaya <srigowri.venkatesha@gmail.com>
2020-08-02 19:04:57 +0530

    created js for carousel

---
+++ assets/js/carousel_custom.js
@@ -0,0 +1,29 @@
+
+var create_carousel = (function (window) {
+
+    var init = function () {
+        var i;
+        var slides = document.getElementsByClassName("carousel_img");
+        var dots = document.getElementsByClassName("carousel_dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" carousel_active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " carousel_active";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+    // Expose methods.
+    return {
+        init: init
+    };
+
+})(window);
+
+// Kickstart Demo.
+window.onload = create_carousel.init;
