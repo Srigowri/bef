888975a9e2dd394f67a187b750be2f56a7790271
Vijaya <srigowri.venkatesha@gmail.com>
2020-08-02 19:57:54 +0530

    invoke on window load

--- assets/js/carousel_custom.js
+++ assets/js/carousel_custom.js
@@ -1,21 +1,22 @@
 
- var startCarousel = function() {
-    var i;
-    var slides = document.getElementsByClassName("carousel_img");
-    var dots = document.getElementsByClassName("carousel_dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" carousel_active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " carousel_active";
-    setTimeout(startCarousel, 2000); // Change image every 2 seconds
-};
 window.onload = function(){
  var slideIndex = 0;
- new startCarousel();
+ startCarousel();
+ 
+    function startCarousel(){
+        var i;
+        var slides = document.getElementsByClassName("carousel_img");
+        var dots = document.getElementsByClassName("carousel_dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" carousel_active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " carousel_active";
+        setTimeout(startCarousel, 2000); // Change image every 2 seconds
+    };
 }
